109b23c84a4a9eb839a0efc78ccd11acb5ae918b
Anna Lai <annateresa.lai@mail.utoronto.ca>
2023-06-12 16:12:46 -0400

    Update Nav links for dev

---
+++ src/pages/AttentionMapPage.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../App.css';
+
+const AttentionMapPage = () => {
+    const pageStyle = {
+    backgroundColor: '#2A52BE',
+    color: 'white',
+    fontFamily: 'ClarityAI',
+    };
+    
+    return <div style={pageStyle}>Your page content goes here</div>;
+    };
+
+export default AttentionMapPage;
