31a18a3c46da1c3639ba86ce89a416cfa7a1a3a3
Jasmine Sun-Hu <jasminesunhu@gmail.com>
2023-06-13 11:17:23 -0400

    DNN data page + images

--- src/pages/DnnDataPage.js
+++ src/pages/DnnDataPage.js
@@ -1,19 +1,27 @@
 import React from 'react';
 import '../App.css';
-
+import FIgraph from '../images/feature-importance.png';
+import FIcorr from '../images/feature-correlation.png';
 
 const DnnDataPage = () => {
+
     const pageStyle = {
-      backgroundColor: 'blue',
-      color: 'white',
-      textAlign: 'center',
-      padding: '20px',
-    };
-  
+        backgroundColor: '#2A52BE',
+        color: 'white',
+        textAlign: 'center',
+        padding: '20px',
+      };
+
     return (
-      <div style={pageStyle}>
-        <h1>Welcome to ClarityAI DNN Data Page</h1>
-      </div>
+        <div style={pageStyle}>
+            <div>
+                <p>Feature importance measures the significance of input features in influencing the predictions of a DNN. Feature correlation quantifies how changes in one feature are related to changes in another feature. Check out our <a href="/info"style={{ color: '#BCF8EC' }}>info page</a> for more details!</p>
+            </div>
+            <div>
+                <img src={FIgraph} alt="Feature Importance Graph" style={{ width: '45%', height: 'auto' , marginRight: '2%' }}/>
+                <img src={FIcorr} alt="Feature Correlation Matrix" style={{ width: '45%', height: 'auto' }}/>
+            </div>
+        </div> 
     );
   };
 
