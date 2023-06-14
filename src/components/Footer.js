9f2625c090bb348882841879b47270922699ef11
Jasmine Sun-Hu <jasminesunhu@gmail.com>
2023-06-14 14:06:12 -0400

    footer homepage link fix

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,6 +2,7 @@ import React from 'react';
 import mail from '../images/mail.png'; 
 import github from '../images/github.png'; 
 import logo from '../images/main-logo.png';
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
 
@@ -17,9 +18,9 @@ const Footer = () => {
     <footer style={footerStyle}>
       <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', marginBottom: '10px' }}>
         <img src={mail} alt="Logo 1" style={{ width: '40px', height: '40px', marginRight: '10px' }} />
-        <a href='/clarity-ai'>
+        <Link to='/clarity-ai'>
           <img src={logo} alt="Logo 2" style={{ width: '30px', height: '30px', marginRight: '17px' }} />
-        </a>
+        </Link>
         <a href="https://github.com/JasmineZhangxyz/clarity-ai" target="_blank" rel="noopener noreferrer">
           <img src={github} alt="Logo 3" style={{ width: '25px', height: '25px' }} />
         </a>
